f9e76359fada98cbb120566302b8836ccb1a453e
Zheng Chen <zheng.chen@gm.com>
2018-11-29 13:53:12 -0500

    Week2 Assignment Submission Part 2

--- Algorithmic Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -7,13 +7,15 @@ def fibonacci_sum_naive(n):
 
     previous = 0
     current  = 1
-    sum      = 1
+    sum      = [0, 1]
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current
-
-    return sum % 10
+    for i in range(n - 1):
+        previous, current = current, (previous + current) % 10
+        if sum[-1] == 0 and current == 1:
+            index = n % (i + 1)
+            return sum[index]
+        sum.append((current + sum[-1]) % 10)
+    return sum[-1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- Algorithmic Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -1,18 +1,24 @@
 # Uses python3
 import sys
 
+def fibonacci_sum_naive(n):
+    if n < 1: return 0
+    if n == 1: return 1
+
+    previous = 0
+    current  = 1
+    sum      = [0, 1]
+
+    for i in range(n - 1):
+        previous, current = current, (previous + current) % 10
+        if sum[-1] == 0 and current == 1:
+            index = n % (i + 1)
+            return sum[index]
+        sum.append((current + sum[-1]) % 10)
+    return sum[-1]
+
 def fibonacci_partial_sum_naive(from_, to):
-    sum = 0
-
-    current = 0
-    next  = 1
-
-    for i in range(to + 1):
-        if i >= from_:
-            sum += current
-
-        current, next = next, current + next
-
+    sum = fibonacci_sum_naive(to) - fibonacci_sum_naive(from_ - 1)
     return sum % 10
 
 
