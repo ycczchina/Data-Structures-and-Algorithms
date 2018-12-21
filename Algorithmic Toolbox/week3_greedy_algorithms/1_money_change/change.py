58d04b61eb7f6a6ceab3032bf24e8287a9e5fac9
ycczchina <chenzhen@umich.edu>
2018-12-21 02:32:04 -0500

    Week3 Assignment Submission Part 1

--- Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -3,7 +3,15 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    count = 0
+    while(m >= 10):
+    	m -= 10
+    	count += 1
+    while(m >= 5):
+    	m -= 5
+    	count += 1
+    count += m
+    return count
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -4,7 +4,14 @@ import sys
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
-
+    data=[(w, v) for w, v in zip(weights,values)] 
+    data.sort(key=lambda x: (-x[1] / x[0]))
+    i = 0
+    while(capacity > 0 and i < len(data)):
+    	a = min(data[i][0], capacity)
+    	value += a * data[i][1] /data[i][0]
+    	capacity -= a
+    	i += 1
     return value
 
 
