293082bf01684f385cbb37d1dadc357bb0f42da0
ycczchina <chenzhen@umich.edu>
2018-12-22 02:00:21 -0500

    Week3 Assignment Submission Part 2

--- Algorithmic Toolbox/week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
@@ -5,17 +5,28 @@ from collections import namedtuple
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
-    points = []
+    left = segments[0][0]
+    right = segments[0][1]
+    points = [right]
     #write your code here
     for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+        if(points[-1] >= s[0] and points[-1] <= s[1]):
+            left = max(left, s[0])
+            right = min(right, s[1])
+        elif(points[-1] > s[1]):
+            right = s[1]
+            points[-1] = right
+        else:
+            points.append(s[1])
+            left = s[0]
+            right = s[1]
     return points
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *data = map(int, input.split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+    segments.sort(key=lambda x: x[0])
     points = optimal_points(segments)
     print(len(points))
     for p in points:
--- Algorithmic Toolbox/week3_greedy_algorithms/5_maximum_number_of_prizes/different_summands.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/5_maximum_number_of_prizes/different_summands.py
@@ -1,9 +1,12 @@
 # Uses python3
 import sys
+import math
 
 def optimal_summands(n):
-    summands = []
     #write your code here
+    x = int((-1 + math.sqrt(1 + 8 * n)) / 2)
+    summands = [i for i in range(1, x)]
+    summands.append(int(n - (x + 1) * x / 2 + x))
     return summands
 
 if __name__ == '__main__':
